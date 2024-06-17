8f7e5c00baf60bb59c1f1cecf3d30ca9afd0051d
shrikant <shrikantsupekar96@gmail.com>
2024-06-17 20:26:37 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="dark:bg-black h-screen w-screen">
+    <div class="preview flex"></div>
+    <div class="timeline"></div>
+  </main>
+</template>
